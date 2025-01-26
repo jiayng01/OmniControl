449b48e04d43e9d414bda092f988049c92cff20a
jia ying <92442809+jiayng01@users.noreply.github.com>
2025-01-26 19:16:26 +0800

    Edit eval scripts for DDIM

--- eval_omnicontrol_all.sh
+++ eval_omnicontrol_all.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 model_path=$1
 # eval root joint
-python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 0 --density 1
-# python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 0 --density 2
-python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 0 --density 5
-python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 0 --density 25
+# python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 0 --density 1
+# # python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 0 --density 2
+# python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 0 --density 5
+# python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 0 --density 25
 python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 0 --density 100
-# eval left foot
-python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 10 --density 1
-# python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 10 --density 2
-python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 10 --density 5
-python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 10 --density 25
-python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 10 --density 100
-# eval right foot
-python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 11 --density 1
-# python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 11 --density 2
-python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 11 --density 5
-python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 11 --density 25
-python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 11 --density 100
+# # eval left foot
+# python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 10 --density 1
+# # python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 10 --density 2
+# python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 10 --density 5
+# python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 10 --density 25
+# python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 10 --density 100
+# # eval right foot
+# python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 11 --density 1
+# # python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 11 --density 2
+# python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 11 --density 5
+# python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 11 --density 25
+# python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 11 --density 100
 # # eval head
 # python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 15 --density 1
 # # python -m eval.eval_humanml --model_path ${model_path} --eval_mode omnicontrol --control_joint 15 --density 2
